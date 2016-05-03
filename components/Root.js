f3e79b94b5f478d08d280b7857db15c7fe5f53ce
Lachlan Campbell <lachlan.campbell@icloud.com>
2016-05-03 15:27:20 -0400

    simpler root structure

--- components/Root.js
+++ components/Root.js
@@ -4,23 +4,20 @@ import Link from './Link'
 import Main from './Main'
 import Footer from './Footer'
 
-class Root extends React.Component {
-  render () {
-    const style = {
-      backgroundColor: '#414e58',
-      color: 'white',
-      padding: '1rem'
-    }
-    return (
-      <html>
-        <Head title='Lachlan Campbell' />
-        <body style={style}>
-          <Main />
-          <Footer />
-        </body>
-      </html>
-    )
-  }
+const style = {
+  backgroundColor: '#414e58',
+  color: 'white',
+  padding: '1rem'
 }
+
+const Root = ({ title }) => (
+  <html>
+    <Head title='Lachlan Campbell' />
+    <body style={style}>
+      <Main />
+      <Footer />
+    </body>
+  </html>
+)
 
 export default Root
